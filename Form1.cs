2353b833c943bed0cebd2f1a97548ca8a8a9652f
Ee0Rk <erikottofrederik@gmail.com>
2022-10-29 19:46:35 +0200

    added project

---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2dRaycastThing
+{
+    public partial class Form1 : Form
+    {
+        List<PointF[]> objects = new List<PointF[]>();
+
+        bool buttonHeld = false;
+
+        PointF location;
+        PointF originalLocation;
+        double angle;
+
+        bool locMove;
+
+        int fov = 60;
+        List<PointF> pnts = new List<PointF>();
+
+        public Form1()
+        {
+            PointF[] points = { new PointF(100, 0), new PointF(0, 150), new PointF(200, 150)};
+            objects.Add(points);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            location = e.Location;
+            angle = util.Angle(originalLocation, location);
+            if (locMove)
+            {
+                pnts = util.multicast(fov, 150f, originalLocation, angle*-1, objects.ToArray());
+                pnts.Add(originalLocation);
+            }
+            this.Refresh();
+            GC.Collect();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (buttonHeld)
+            {
+                e.Graphics.DrawLine(Pens.Black, location, originalLocation);
+
+                e.Graphics.DrawString(Math.Round(angle).ToString(),SystemFonts.DefaultFont,Brushes.Black, originalLocation);
+                for (int i = 0; i < fov; i++)
+                {
+                    e.Graphics.DrawLine(Pens.Green, pnts[i], originalLocation);
+                }
+                if (locMove)
+                {
+                    
+                    e.Graphics.DrawPolygon(Pens.Green, pnts.ToArray());
+                    e.Graphics.DrawString(pnts.Count.ToString(), SystemFonts.DefaultFont, Brushes.Black, location);
+                    e.Graphics.DrawString(string.Join(",\n", pnts), SystemFonts.DefaultFont, Brushes.Black, Point.Empty);
+                }
+            }
+            foreach (PointF[] obj in objects)
+            {
+                e.Graphics.DrawPolygon(Pens.Black, obj);
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            originalLocation = location;
+            locMove = true;
+            buttonHeld = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            originalLocation = PointF.Empty;
+            locMove = false;
+            buttonHeld = false;
+        }
+    }
+}
---
+++ util.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace _2dRaycastThing
+{
+    public static class util
+    {
+        const double Rad2Deg = 180.0 / Math.PI;
+        const double Deg2Rad = Math.PI / 180.0;
+        public static double Angle(PointF start, PointF end)
+        {
+            return Math.Atan2(start.Y - end.Y, end.X - start.X) * Rad2Deg;
+        }
+
+        public static PointF MoveInDirection(PointF _from, float distance, double direction)
+        {
+            double new_x = _from.X + distance * Math.Cos(direction * Math.PI / 180);
+            double new_y = _from.Y + distance * Math.Sin(direction * Math.PI / 180);
+            PointF value = new PointF((int)Math.Round(new_x), (int)Math.Round(new_y));
+            return value;
+        }
+
+        public static bool IsPointInPolygon(PointF[] polygon, PointF point)
+        {
+            bool isInside = false;
+            for (int i = 0, j = polygon.Length - 1; i < polygon.Length; j = i++)
+            {
+                if (((polygon[i].Y > point.Y) != (polygon[j].Y > point.Y)) &&
+                (point.X < (polygon[j].X - polygon[i].X) * (point.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) + polygon[i].X))
+                {
+                    isInside = !isInside;
+                }
+            }
+            return isInside;
+        }
+
+        public static List<PointF> multicast(int fov, float range, PointF location, double angle, PointF[][] objects, float precision=0.1f)
+        {
+            double ang;
+            List<PointF> arr = new List<PointF>();
+            float f = (fov / 2) * -1;
+            for (int i = 0; i < fov; i++)
+            {
+                ang = angle + (double)f;
+                PointF pnt = raycast(objects,location, ang, range, precision);
+                arr.Add(pnt);
+                f++;
+            }
+            return arr;
+        }
+
+        public static PointF raycast(PointF[][] objects, PointF startLocation, double angle, float maxDist, float step)
+        {
+            PointF point = new PointF();
+            for (float i = 0; i < maxDist; i = i+step)
+            {
+                point = MoveInDirection(startLocation, i, angle);
+                foreach (PointF[] objec in objects)
+                {
+                    if (IsPointInPolygon(objec,point))
+                    {
+                        return point;
+                    }
+                }
+            }
+            return point;
+        }
+    }
+    
+}
